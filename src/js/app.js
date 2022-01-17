b6c5e4ca260e6d52cf2a5b158537da4304f1c9b5
George Mihov <gmihov001@gmail.com>
2022-01-17 17:31:58 +0000

    Initial

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,18 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -46,20 +23,12 @@ function render(variables = {}) {
     `;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
     github: "alesanchezr",
     linkedin: null,
@@ -70,22 +39,32 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); //render the card for the first time
+
+  render(window.variables);
 
   document.querySelectorAll(".picker").forEach(function(elm) {
+    // for every element .picker do something
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
+      // add event listener on change, gets back the event as param
+
+      const attribute = e.target.getAttribute("for"); // set attribute to be the value of the 'for' attribute of each .picker lement, eg. "lastname", "instagram", "linkedin", etc
+
+      let values = {}; // new empty object
+      console.log("this.value", this.value);
+      console.log("this", this);
+      console.log("e", e);
+      console.log("elm", elm.value);
+
+      values[attribute] = //create a key in values object with each attribute for's value
+        this.value == "" || this.value == "null" //if value is empty or null - return null for that key
           ? null
-          : this.value == "true"
+          : this.value == "true" //if value is true - assign true to key
           ? true
-          : this.value == "false"
+          : this.value == "false" //if value is false - assign false to key
           ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new valus
+          : this.value; //in all other cases assign the value
+
+      render(Object.assign(window.variables, values));
     });
   });
 };
